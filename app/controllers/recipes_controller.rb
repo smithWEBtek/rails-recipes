a017b51e798e1464ef0a5b714f39c1f9391f4ede
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-11-08 09:32:33 -0500

    strong params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,8 +4,17 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+  end
+
+
+
+private
+  def recipe_params
+    params.require(:recipe).permit(:title, :instructions)
   end
 end
