9bd8a5e87f1382c421b3427405ea8707e690f2bc
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-11-10 16:22:08 -0500

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
