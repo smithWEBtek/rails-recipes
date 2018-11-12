f69d436046fd9dc4efa3c0921524bbfb17048d84
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-11-12 12:06:02 -0500

    fix ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,12 +2,17 @@ class IngredientsController < ApplicationController
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    @ingredient = @recipe.ingredients.create(params[:ingredients]).permit(:name, :quantity)
+    @ingredient = @recipe.ingredients.create(ingredient_params)
     redirect_to recipe_path(@recipe)
 
   end
 
   def destroy
 
+  end
+
+private
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :quantity)
   end
 end
