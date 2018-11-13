1af19a97072bd9d39303813e050d18c03f66ccf9
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-11-13 13:54:53 -0500

    build out next and previous buttons for recipes

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -2,3 +2,8 @@
 //   var nextId = parseInt($(".js-next").attr("data-id")) + 1
 //   updateView(nextId)
 // }
+//
+// function previousRecipe() {
+//   var prevId = parseInt($(".js-previous").attr("data-id")) - 1
+//   updateView(prevId)
+// }
