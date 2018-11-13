6a9c2423f852d16f175dc1bef2f397bf005748c7
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-11-12 21:20:06 -0500

    correctly render json for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,10 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all.order("title ASC")
-    render json: @recipes
+    respond_to do |f|
+      f.html
+      f.json {render json: @recipes}
+    end
   end
 
   def new
